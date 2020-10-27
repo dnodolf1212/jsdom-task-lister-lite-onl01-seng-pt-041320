6538d0127a36c02c2620f1242bc095487fad0f96
dnodolf1212 <dnodolf@gmail.com>
2020-10-26 22:52:04 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  // addEventListener() for submit button (submit event on form element)
+  // use preventDefault() function for submit event
+  // function for adding html text string to TO DOs Box
+  // button appended to text string to delete it
+  const jobForm = document.querySelector('#create-task-form');
+  const jobsList = document.querySelector('#tasks');
+
+  jobForm.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+
+    const jobInput = document.querySelector('#new-task-description').value;
+    jobsList.innerHTML += `
+      <li> ${jobInput} 
+        <button data-action="delete"> Dunnit </button>
+      </li>`
+    
+    jobForm.reset()
+
+  })
+
+  jobsList.addEventListener('click', function(e) {
+    console.log(e.target)
+    if (e.target.dataset.action === "delete") {
+      e.target.parentElement.remove();
+    }
+    
+  })
+}) 
+
+//var curSubmit = curForm.querySelector('button[type="submit"]');
+//const jobItem = document.createElement('li');
+    //jobItem.innerText = jobInput;
+    //jobsList.appendChild(jobItem)
